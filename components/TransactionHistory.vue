91db2915dcf957af81b4b547a47c3f06e2a1dd6d
Kenneth Solomon <synecderz@gmail.com>
2022-08-05 23:17:08 +0800

    customer information and transaction history (WIP)

---
+++ components/TransactionHistory.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <template>
+      <v-data-table
+        :headers="headers"
+        :items="transactions"
+        :items-per-page="5"
+        class="elevation-1"
+      ></v-data-table>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    transactions: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Transaction ID',
+          align: 'start',
+          sortable: false,
+          value: 'id',
+        },
+        { text: 'Orders', value: 'orders' },
+        { text: 'Credit', value: 'credit' },
+        { text: 'Discount', value: 'discount' },
+        { text: 'Employee', value: 'employee_info.fullname' },
+        { text: 'Status', value: 'status' },
+        { text: 'Last Transaction', value: 'latest_transaction' },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped></style>
--- pages/customers/_customerid.vue
+++ pages/customers/_customerid.vue
@@ -1,49 +1,82 @@
 <template>
-  <v-card color="basil">
-    <v-card-title class="text-center justify-center py-6">
-      <h1 class="font-weight-bold text-h2 basil--text">
-        {{ transactions.fullname }}
-      </h1>
-    </v-card-title>
+  <div>
+    <v-card>
+      <v-btn
+        style="float: right"
+        class="ma-3"
+        color="primary"
+        @click="$router.push(`/customers`)"
+        ><v-icon class="mr-2">mdi-keyboard-backspace</v-icon> Back</v-btn
+      >
+      <v-card-title class="text-center justify-center py-6">
+        <h1 class="font-weight-bold text-h2 basil--text">
+          {{ transactions.fullname }}
+        </h1>
+      </v-card-title>
 
-    <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
-      <v-tab>Customer Information</v-tab>
-      <v-tab>Transaction List</v-tab>
-      <v-tab>Borrowed Items</v-tab>
-    </v-tabs>
+      <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
+        <v-tab>Customer Information</v-tab>
+        <v-tab>Transaction History</v-tab>
+      </v-tabs>
 
-    <v-tabs-items v-model="tab">
-      <v-tab-item>
-        <v-card color="basil" flat>
-          <v-card-text>
-            <h3>Fullname: {{ transactions.fullname }}</h3>
-            <h3>Address: {{ transactions.address }}</h3>
-            <h3>Contact Number: {{ transactions.contact_number }}</h3>
-            <h3>Total Credits: {{ computeCredit }} Pesos</h3>
-            <h3>
-              Total Borrowed Items:
-              {{ transactions.borrows.length }} Borrowed Items
-            </h3>
-            <h3>
-              Total Transactions:
-              {{ transactions.transactions.length }} Transactions
-            </h3>
-            <h3>Customer Since: {{ transactions.register_customer }}</h3>
-          </v-card-text>
-        </v-card>
-      </v-tab-item>
-      <v-tab-item>
-        <v-card color="basil" flat>
-          <v-card-text>Transaction List</v-card-text>
-        </v-card>
-      </v-tab-item>
-      <v-tab-item>
-        <v-card color="basil" flat>
-          <v-card-text>Borroed Items</v-card-text>
-        </v-card>
-      </v-tab-item>
-    </v-tabs-items>
-  </v-card>
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <v-card flat>
+            <v-card-text>
+              <v-row class="pa-5">
+                <v-col cols="2" class="pa-0">
+                  <h3>Address:</h3>
+                </v-col>
+                <v-col cols="10" class="pa-0">
+                  <h3>{{ transactions.address }}</h3>
+                </v-col>
+                <v-col cols="2" class="pa-0">
+                  <h3>Contact Number:</h3>
+                </v-col>
+                <v-col cols="10" class="pa-0">
+                  <h3>{{ transactions.contact_number }}</h3>
+                </v-col>
+                <v-col cols="2" class="pa-0">
+                  <h3>Total Credits:</h3>
+                </v-col>
+                <v-col cols="10" class="pa-0">
+                  <h3>{{ computeCredit }}</h3>
+                </v-col>
+                <v-col cols="2" class="pa-0">
+                  <h3>Total Borrowed Items:</h3>
+                </v-col>
+                <v-col cols="10" class="pa-0">
+                  <h3>{{ transactions.borrows.length }}</h3>
+                </v-col>
+                <v-col cols="2" class="pa-0">
+                  <h3>Total Transactions:</h3>
+                </v-col>
+                <v-col cols="10" class="pa-0">
+                  <h3>{{ transactions.transactions.length }}</h3>
+                </v-col>
+
+                <v-col cols="2" class="pa-0">
+                  <h3>Customer Since:</h3>
+                </v-col>
+                <v-col cols="10" class="pa-0">
+                  <h3>{{ transactions.register_customer }}</h3>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <v-card-text>
+              <TransactionHistory
+                :transactions="transactions.transactions"
+              ></TransactionHistory>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+  </div>
 </template>
 
 <script>
