c7626cf306ed34c392c43e5badf9b18f78b8e8dc
Kenneth Solomon <synecderz@gmail.com>
2022-08-06 11:54:02 +0800

    transaction history order list

--- components/TransactionHistory.vue
+++ components/TransactionHistory.vue
@@ -5,8 +5,62 @@
         :headers="headers"
         :items="transactions"
         :items-per-page="5"
+        sort-by="latest_transaction"
+        sort-desc
         class="elevation-1"
-      ></v-data-table>
+      >
+        <template v-slot:[`item.orders`]="{ item }">
+          <v-menu
+            :close-on-content-click="false"
+            open-on-hover
+            :nudge-width="200"
+            offset-y
+            bottom
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <span v-bind="attrs" v-on="on">
+                <v-icon style="cursor: pointer" color="primary"
+                  >mdi-cart</v-icon
+                >
+              </span>
+            </template>
+            <v-card flat>
+              <v-simple-table dense>
+                <template v-slot:default>
+                  <h4 class="mx-3">Order List:</h4>
+                  <tbody v-for="(order, index) in item.orders" :key="index">
+                    <tr>
+                      <th>Item:</th>
+                      <td>{{ order.item_info.name }}</td>
+
+                      <th>Quantity:</th>
+                      <td>{{ order.quantity }}</td>
+                      <th>Type of Service:</th>
+                      <td>
+                        {{
+                          order.type_of_service.charAt(0).toUpperCase() +
+                          order.type_of_service.slice(1)
+                        }}
+                      </td>
+                      <th>Borrow:</th>
+                      <td>{{ Number(order.is_borrow) ? '✅' : '❌' }}</td>
+                      <th>Purchase:</th>
+                      <td>{{ Number(order.is_purchase) ? '✅' : '❌' }}</td>
+                      <th>Free:</th>
+                      <td>{{ Number(order.is_free) ? '✅' : '❌' }}</td>
+                    </tr>
+                    <v-divider></v-divider>
+                  </tbody>
+                </template>
+              </v-simple-table>
+            </v-card>
+          </v-menu>
+        </template>
+
+        <template v-slot:[`item.status`]="{ item }">
+          {{ item.status.charAt(0).toUpperCase() + item.status.slice(1) }}
+        </template>
+      </v-data-table>
     </template>
   </div>
 </template>
